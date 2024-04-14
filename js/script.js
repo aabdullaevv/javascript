42ae377cb726a2a090bad702586d56356b438dc1
Abdullaev008 <aabdullaev.web@gmail.com>
2024-04-14 11:15:11 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,167 @@
+"use strict"; //code degi xatolarni korsatadi, buni eng tepaga yozip qoyish kere
+
+// const number = 10;
+// console.log(number);
+
+// let number = 16;
+// console.log(number);
+
+// number = 20;
+// console.log(number);
+
+
+// const borderColor = 'red';
+// console.log(borderColor);
+
+
+
+// const person = {
+//   name: 'Azizbek'
+// };
+
+// person.name = 'Eldor';
+// console.log(person)
+
+
+// `` -- beksik
+
+// var clientName = "Azizbek";
+// console.log(clientName);
+
+
+
+// DATA TYPES
+
+// Number
+// let number = 5.6; // Data Type = Number
+
+// console.log(8 / 0); // Infinity
+
+// console.log("Azizbek" * 8); // NaN -- not a number
+
+
+// STRING
+// const clientName = "AZIZBEK";
+
+//  Boolean
+// const isMarried = false;
+// const isCircleEarth = true;
+
+// null
+// console.log(clientAge);
+
+
+// undefined
+// let und;
+// console.log(und);
+
+
+// Object
+// const thief = {
+// key: value
+// age: 30, //num
+// jacket: "black", //string
+// Long: false, //boolean
+// };
+// console.log(thief);
+
+// Array
+//                  0         1         2       3   4   5 -- key
+// const colors = ["red", "black", "green", 10, [], {}]; // --value
+// console.log(colors[0]);
+
+
+// Alert
+// alert("Hello Azizbek");
+
+// const place = confirm("Are you here?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?", "Shaxar");
+// console.log(typeof currentPlace);
+
+// const age = +prompt("How old are you?", "18");
+// console.log(typeof age);
+
+
+
+
+// const favouriteColor = [];
+
+// favouriteColor[0] = prompt("What's your favourite color? #1", "");
+// favouriteColor[1] = prompt("What's your favourite color? #2", "");
+// favouriteColor[2] = prompt("What's your favourite color? #3", "");
+
+// console.log(favouriteColor);
+
+
+// const favouriteFood = [];
+
+// favouriteFood[0] = prompt("What's your favourite food? #1", "");
+// favouriteFood[1] = prompt("What's your favourite food? #2", "");
+// favouriteFood[2] = prompt("What's your favourite food? #3", "");
+// console.log(favouriteFood)
+
+
+
+// kanaldi topish
+// const channel = prompt("What's your favourite youtuber?");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Azizbek";
+// const job = "JavaScript Developer";
+// console.log(`User name is ${user}, he is ${job}`);
+
+
+// const productName = "apple";
+// const productPrice = "10$";
+// console.log(`Product name is ${productName}, price is ${productPrice}`);
+
+
+
+//OPERATORLAR
+
+// console.log(6 + "9");
+
+// incr, decr
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+
+
+// console.log(10 % 4);
+
+
+// console.log(5 * 5 == "25");
+// console.log(5 * 5 === "25");
+
+
+// && = va || = yoki
+
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log(isAge && isClose && !isChecked);
+
+
+// const button = document.querySelector('button');
+// button.addEventListener('click', updateName);
+
+// function updateName() {
+//   const name = prompt('Enter a new name');
+//   button.textContent = `Player 1: ${name}`;
+// }
+
+
+console.log(2 + 2 * 2 !== 6);
+
+
+
